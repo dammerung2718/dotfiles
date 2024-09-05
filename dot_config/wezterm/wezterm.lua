a0428763184c2a16113f0278da845ba8d61c1d3d
dammerung2718 <dammerung2718@icloud.com>
2024-09-05 16:06:14 +0300

    formatted files

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,12 +1,12 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- theme
-config.color_scheme = 'tokyonight'
+config.color_scheme = "tokyonight"
 config.hide_tab_bar_if_only_one_tab = true
 
 -- font
-config.font = wezterm.font 'Departure Mono'
+config.font = wezterm.font("Departure Mono")
 config.font_size = 22.0
 
 return config
