60a4be98bcbf66429c67bec50383efb48e5bea01
dammerung2718 <dammerung2718@icloud.com>
2024-10-01 04:45:36 +0300

    replaced zellij with tmux

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -13,6 +13,7 @@ return {
     -- setup servers
     lspconfig.rust_analyzer.setup({})
     lspconfig.pyright.setup({})
+    lspconfig.gopls.setup({})
 
     -- setup keymaps
     local opts = { buffer = bufnr, noremap = true, silent = true }
@@ -33,5 +34,6 @@ return {
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
     vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
   end,
 }
